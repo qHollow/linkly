d263028e296d384eb0baed7718484b931e5d1a99
qHollow <nighthunter2015@gmail.com>
2023-06-11 17:27:50 +0300

    feat(ui): change colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ru" className="dark-theme">
+		<html lang="ru" data-theme="dark">
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
